cf905018c8169c5cc708281db494c9f772e7d0de
a.paris <adrien.paris3@gmail.com>
2022-01-06 17:35:52 +0100

    Sauvegarde de fin de journ├®e

---
+++ sandbox/ctrlVisSwitch.py
@@ -0,0 +1,466 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+
+
+"""ctrlVisSwitch.py: Just an interface to switch the visibility of ctrls"""
+
+__author__      = "Adrien PARIS"
+__email__       = "a.paris.cs@gmail.com"
+__version__     = "1.0.0-alpha"
+__copyright__   = "Copyright 2021, Creative Seeds"
+
+import sys
+import ctypes
+import threading
+try:
+    import maya.cmds as cmds
+    import maya.mel as mel
+except:
+    pass
+
+class Callback():
+    '''Use for maya interface event, because it send you back your argument as strings
+    func : the function you want to call
+    *args : your arguments
+    **kwargs : your keywords arguments
+
+    Example:
+    cmds.button(c=Callback(myFunc, arg1, arg2))
+    cmds.button(c=Callback(myFunc, arg1, arg2).repeatable())
+    '''
+    __author__ = "Adrien PARIS"
+    __email__ = "a.paris.cs@gmail.com"
+    __version__     = "3.1.0"
+    __copyright__   = "Copyright 2021, Creative Seeds"
+    
+    def __init__(self, func, *args, **kwargs):
+        self.func = func
+        self.args = args
+        self.repeatArgs = args
+        self.kwargs = kwargs
+        self.repeatable_value = False
+        self.getCommandArgument_value = False
+
+    def _addArgument(self, *args, **kwargs):
+        self.args = args
+        self.repeatArgs = args
+        self.kwargs = kwargs
+        return self
+        
+    def repeatable(self):
+        ''' Call this methode to make the function repeatable with 'g'
+        '''
+        self.repeatable_value = True
+        return self
+
+    def getCommandArgument(self):
+        ''' Call this methode to receive the argument of the event
+        '''
+        self.getCommandArgument_value = True
+        return self
+        
+    def _repeatCall(self):
+        return self.func(*self.repeatArgs, **self.kwargs)
+
+    def __call__(self, *args):
+        ag = self.args + args if self.getCommandArgument_value else self.args
+        if self.repeatable_value:
+            import __main__
+            __main__.cb_repeatLast = self
+            self.repeatArgs = ag
+            cmds.repeatLast(ac='''python("import __main__; __main__.cb_repeatLast._repeatCall()"); ''')
+        return self.func(*ag, **self.kwargs)
+
+# Decorator
+def callback(func):
+    def wrapper(*args, **kwargs):
+        return Callback(func, *args, **kwargs)
+    return wrapper
+
+def thread(func):
+    def wrapper(*args, **kwargs):
+        t = threading.Thread(target=func, args=args, kwargs=kwargs)
+        t.start()
+    return wrapper
+
+def singleton(class_):
+    class class_w(class_):
+        __doc__ = class_.__doc__
+        _instance = None
+        def __new__(class_, *args, **kwargs):
+            if class_w._instance is None:
+                class_w._instance = super(class_w,
+                                    class_).__new__(class_,
+                                                    *args,
+                                                    **kwargs)
+                class_w._instance._sealed = False
+            return class_w._instance
+        def __init__(self, *args, **kwargs):
+            if self._sealed:
+                return
+            super(class_w, self).__init__(*args, **kwargs)
+            self._sealed = True
+    class_w.__name__ = class_.__name__
+    return class_w
+
+class Module(object):
+    '''Little bloc to encapsulate the UI's maya's system
+    The parent must be either a Module, either a string that has the name of a maya's layout
+    (cmds.layout, cmds.formLayout, cmds.columnLayout, ...)
+
+    When converting this object to str, it print the layout to store his childrens
+    like that, even if whe call it has a parent, it still return a maya's layout
+    
+    You must create a load function.
+    
+    Name your class starting by
+    M_  if it's a simple module
+    MG_ if it's a group of module
+    MT_ if it's for a specific tools
+    MC_ if it's for a control
+    MGT_ if it's a group that contain tools
+    '''
+    # Maya's colors
+    COLOR_WHITE = [1, 1, 1]
+    COLOR_BLACK = [0, 0, 0]
+    COLOR_BLUE = [0.32, 0.52, 0.65]
+    COLOR_TURQUOISE = [0.28, 0.66, 0.70]
+    COLOR_ORANGE = [0.86, 0.58, 0.34]
+    COLOR_DARKGREY = [0.21, 0.21, 0.21]
+    COLOR_GREY = [0.26, 0.26, 0.26]
+    COLOR_LIGHTGREY = [0.36, 0.36, 0.36]
+    COLOR_GREEN = [0.48, 0.67, 0.27]
+    COLOR_RED = [0.85, 0.34, 0.34]
+    COLOR_YELLOW = [0.86,0.81,0.53]
+
+
+    increment = 0
+    drag = None
+
+    def __init__(self, parent, name=None):
+        
+        if name is None:
+            name = self.__class__.__name__ + str(Module.increment)
+            Module.increment += 1
+        self.name = name
+        self.childrens = []
+        self.command = {}
+        self.parent = None
+        self.setParent(parent)
+        self._scriptJobIndex = []
+        self.childrenLayout = None
+        self.layout = None
+        self.bgc = None
+        self.bgc = False
+        self.dragged = False
+        self.af = []
+        self.ac = []
+        self.ap = []
+
+    def __str__(self):
+        return str(self.childrenLayout)
+
+    def __getattribute__(self, name):
+        if name == 'load':
+            return object.__getattribute__(self, "_load")
+        if name == "height":
+            return cmds.layout(self.layout, q=True, h=True)
+        if name == "width":
+            return cmds.layout(self.layout, q=True, w=True)
+        return object.__getattribute__(self, name)
+
+    def setParent(self, parent):
+        if self.parent is not None:
+            if isinstance(self.parent, Module):
+                self.parent.childrens.remove(self)
+        self.parent = parent
+        if isinstance(self.parent, Module):
+            self.parent.childrens.append(self)
+
+    def attach(self, elem, top=None, bottom=None, left=None, right=None, margin=(0,0,0,0)):
+        '''For formLayout
+            Register the attach information of your elem 
+            attach "FORM": string
+                    elem : layout/control
+                    pos : float
+                    None
+            return elem
+            Use applyAttach() to attach the layout to your parent
+        '''
+        if isinstance(elem, Module):
+            e = elem.layout
+        else:
+            e = elem
+        for s, n, m in [(top, "top", margin[0]), (bottom, "bottom", margin[1]), (left, "left", margin[2]), (right, "right", margin[3])]: 
+            if s == None:
+                continue
+            if isinstance(s, (str, unicode)):
+                if s.upper() == "FORM":
+                    self.af.append((e, n, m))
+                else:
+                    self.ac.append((e, n, m, s))
+            if isinstance(s, Module):
+                self.ac.append((e, n, m, s.layout))
+            if isinstance(s, (float, int)):
+                self.ap.append((e, n, m, float(s)))
+        return elem
+
+    def clearAttach(self):
+        self.af = []
+        self.ac = []
+        self.ap = []
+    
+    @staticmethod
+    def _getParentLayout(attachList):
+        parLayout = {}
+        for af in attachList:
+            e = af[0]
+            if isinstance(e, Module):
+                parLayout[e.parent] = [af] if e.parent not in parLayout else parLayout[e.parent] + [af]
+            else:
+                if cmds.layout(e, exists=True):
+                    parent = cmds.layout(e, q=True, p=True)
+                elif cmds.control(e, exists=True):
+                    parent = cmds.control(e, q=True, p=True)
+                else:
+                    continue
+                parLayout[parent] = [af] if parent not in parLayout else parLayout[parent] + [af] 
+        return parLayout
+
+    def applyAttach(self):
+        parLayoutAf = Module._getParentLayout(self.af)
+        parLayoutAc = Module._getParentLayout(self.ac)
+        parLayoutAp = Module._getParentLayout(self.ap)
+        parentlayouts = parLayoutAf.keys() + parLayoutAc.keys() + parLayoutAp.keys()
+        parentlayouts = list(dict.fromkeys(parentlayouts))
+        for pl in parentlayouts:
+            af = parLayoutAf[pl] if pl in parLayoutAf else []
+            ac = parLayoutAc[pl] if pl in parLayoutAc else []
+            ap = parLayoutAp[pl] if pl in parLayoutAp else []
+            cmds.formLayout(pl, e=True, af=af, ac=ac, ap=ap)
+
+    def _load(self):
+        object.__getattribute__(self, "load")()
+        cmds.scriptJob(ro=True, uid=[self.layout, Callback(self._killJobs)])
+        cmds.scriptJob(ro=True, uid=[self.layout, Callback(self.unload)])
+        self._loadJobs()
+        return self
+
+    def load(self):
+        ''' Put your main layout in the self.layout
+        If you use a formeLayout use self.attach()
+        Put the layout where the childrens will go in self.childrenLayout
+        '''
+        raise Exception('load function not implemented')
+
+    def reload(self):
+        self.unload()
+        self.load()
+
+    def _unload(self):
+        if self.layout == None:
+            return self
+        if cmds.workspaceControl(self.layout, exists=1):
+            cmds.deleteUI(self.layout)
+        return self
+    def unload(self):
+        pass
+
+    def refresh(self):
+        pass
+
+    def move(self, other):
+        parent = self.parent
+        pos = parent.childrens.index(other)
+        parent.childrens.remove(self)
+        parent.childrens.insert(pos, self)
+        parent.refresh()
+        # tmp = cmds.formLayout(p=parent.layout)
+        # for c in parent.childrens:
+        #     cmds.layout(c.layout, e=True, p=tmp)
+        # for c in parent.childrens:
+        #     cmds.layout(c.layout, e=True, p=parent.childrenLayout)
+        #     # c.reload()
+        # cmds.deleteUI(tmp)
+
+    # Jobs
+    def _loadJobs(self):
+        '''Load all jobs
+        '''
+        # Example : 
+        # self._scriptJobIndex.append(cmds.scriptJob(event=["SceneOpened", Callback(self.methode)]))
+        # raise Exception('_loadJobs function not implemented')
+    def _killJobs(self):
+        '''Kill all jobs
+        '''
+        for i in self._scriptJobIndex:
+            cmds.scriptJob(kill=i, f=True)
+        self._scriptJobIndex = []
+
+    # drag&Drop
+    def _dragCb(self, dragControl, x, y, modifiers):
+        Module.drag = self
+        
+        if not self.dragged:
+            self.bgc = cmds.layout(self.layout, q=True, bgc=True)
+            self.ebg = cmds.layout(self.layout, q=True, ebg=True)
+        self.dragged = True
+        cmds.layout(self.layout, e=True, ebg=True)
+        cmds.layout(self.layout, e=True, bgc=Module.COLOR_BLUE)
+    def _dropCb(self, dragControl, dropControl, messages, x, y, dragType):
+        cmds.layout(Module.drag.layout, e=True, bgc=Module.drag.bgc)
+        cmds.layout(Module.drag.layout, e=True, ebg=Module.drag.ebg)
+        Module.drag.dragged = False
+        Module.drag.move(self)
+        Module.drag = None
+
+    # Events
+    def eventHandler(self, event, c, *args):
+        if not event in self.command:
+            self.command[event] = []
+        self.command[event].append((c, args))
+    def runEvent(self, event, *args):
+        if not event in self.command:
+            return
+        for c in self.command[event]:
+            if c[0] is None:
+                continue
+            a = c[1] + args
+            c[0](*a)
+
+def info(message):
+    mel.eval('trace -where ""; print "{}"; trace -where "";'.format(message))
+
+@singleton
+class SwitchVisibility(Module):
+    THEME_BUTTON = Module.COLOR_LIGHTGREY
+    THEME_SELECTED = Module.COLOR_BLUE
+
+    class Categorie(Module):
+        def __init__(self, parent, name, attrs):
+            Module.__init__(self, parent)
+            self.name = name
+            self.attrs = [[a, False] for a in attrs]
+
+        def setColor(self, attr, v):
+            pos = next((i for i, item in enumerate(self.attrs) if item[0] == attr), -1)
+            # pos = self.attrs.index(attr)
+            self.attrs[pos][1] = v
+            color = SwitchVisibility.THEME_SELECTED if self.attrs[pos][1] else SwitchVisibility.THEME_BUTTON
+            cmds.control(self.btns[attr], e=True, bgc=color)
+
+        def load(self):
+            self.layout = cmds.formLayout(parent=self.parent, bgc=Module.COLOR_DARKGREY)
+            self.title = self.attach(cmds.button(p=self.layout, l=self.name, c=Callback(self.runEvent, "switchAll")))
+            prev = self.title
+            self.btns = {}
+            for a in self.attrs:
+                prev = self.attach(cmds.button(p=self.layout, l=a[0], c=Callback(self.runEvent, "switch", a[0])), top=prev, left="FORM", right="FORM", margin=(3,3,3,3))
+                self.btns[a[0]] = prev
+            self.applyAttach()
+
+    class AssetVis(Module):
+        def __init__(self, parent, ctrl):
+            Module.__init__(self, parent)
+            self.ctrl = ctrl
+            self.categories = self.getCat(ctrl)
+
+        def getCat(self, name):
+            attrs = cmds.listAttr(name)
+            i = attrs.index("rotationInterpolation") + 1
+            attrs = attrs[i:]
+            current = None
+            cat = {}
+            l = []
+            for a in attrs:
+                if cmds.getAttr("{}.{}".format(self.ctrl, a), type=True) != "bool":
+                    if current is not None:
+                        cat[current] = l
+                    l = []
+                    current = a
+                else:
+                    l.append(a)
+            if current is not None:
+                cat[current] = l
+            return cat
+
+        def switch(self, cat, attr):
+            pos = next((i for i, item in enumerate(cat.attrs) if item[0] == attr), -1)
+            v = not cat.attrs[pos][1]
+            cat.setColor(attr, v)
+            cmds.setAttr("{}.{}".format(self.ctrl, attr), v)
+
+        def switchAll(self, cat):
+            hide = False
+            for a, v in cat.attrs:
+                if not v:
+                    hide = True
+                    break
+            for a, v in cat.attrs:
+                cat.setColor(a, hide)
+                cmds.setAttr("{}.{}".format(self.ctrl, a), hide)
+
+        def initButton(self):
+            for char, cat in self.categoriesLays.items():
+                for a in cat.attrs:
+                    v = cmds.getAttr("{}.{}".format(self.ctrl, a[0]))
+                    cat.setColor(a[0], v)
+
+        def load(self):
+            self.layout = cmds.formLayout(parent=self.parent, bgc=Module.COLOR_GREY)
+            self.categoriesLays = {}
+            prev = "FORM"
+            for cat, attrs in self.categories.items():
+                prev = self.attach(SwitchVisibility.Categorie(self.layout, cat, attrs).load(), top=prev, left="FORM", right="FORM", margin=(8,8,3,3))
+                prev.eventHandler("switch", self.switch, prev)
+                prev.eventHandler("switchAll", self.switchAll, prev)
+                self.categoriesLays[cat] = prev
+            self.initButton()
+            
+            self.applyAttach()
+
+    def __init__(self):
+        Module.__init__(self, None)
+        self.name = "{} V{}".format(str(self.__class__.__name__), __version__)
+
+    def load(self):
+        '''loading The window
+        '''
+        # Creating Windows if not exist or reload it
+        if cmds.workspaceControl(self.name, exists=1):
+            cmds.deleteUI(self.name)
+        self.win = cmds.workspaceControl(self.name, ih=100, iw=500, retain=False, floating=True, h=100, w=500)
+        self.layout = cmds.formLayout("Publisher_layout",parent=self.win)
+        self.childrenLayout = self.attach(cmds.formLayout("Publisher_childLay", parent=self.layout), top="FORM", bottom="FORM", left="FORM", right="FORM", margin=(0,0,0,0))
+
+        self.attach(SwitchVisibility.AssetVis(self, "C1:c_visibility").load(), top="FORM", bottom="FORM", left="FORM", right="FORM", margin=(0,0,0,0))
+
+        self.applyAttach()
+        info("[{}] -> loaded!".format(self.name))
+        return self
+
+    def _unload(self):
+        if self.win == None:
+            return self
+        if cmds.workspaceControl(self.win, exists=1):
+            cmds.deleteUI(self.win)
+        return self
+
+if __name__ == "__main__":
+    if sys.executable.endswith(u"bin\maya.exe"):
+        SwitchVisibility().load()
+    else:
+        ctypes.windll.user32.MessageBoxW(0, "Version : {}\n\nJust drag&drop this file to maya's viewport\n\n{}".format(__version__, __doc__), "{} info".format(__file__), 0)
+
+def onMayaDroppedPythonFile(*args):
+    '''Just to get rid of the anoying warning message of maya
+    '''
+    SwitchVisibility().load()
+
+def initializePlugin(*args):
+    '''To load the tool as a plugin
+    '''
+    SwitchVisibility().load()
+
+def uninitializePlugin(*args):
+    SwitchVisibility().unload()
